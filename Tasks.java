87b627999691449b05af3c3846347a68da149c0b
poornasyamasundar <70936332+poornasyamasundar@users.noreply.github.com>
2020-11-02 19:01:08 +0530

    Delete Tasks.java

--- Tasks.java
+++
@@ -1,6 +0,0 @@
-public interface Tasks {
-	public void withdrawCash( BankCustomer customer );
-	public void depositCash( BankCustomer customer );
-	public void changePin( BankCustomer customer );
-	public void showDetails( BankCustomer customer );
-}
